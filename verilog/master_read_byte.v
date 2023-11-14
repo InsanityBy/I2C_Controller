0e8ca4b9088967c109c5634da6ec7796cd78aa21
insanity_by <insanity_by@outlook.com>
2023-11-14 19:24:05 +0800

    add byte-level code and testbench for I2C master reading

--- verilog/master_read_byte.v
+++ verilog/master_read_byte.v
@@ -1,31 +1,54 @@
-module I2C_master_read_bit (
+module I2C_master_read_byte (
     // signal to control module
     input       clock,
     input       reset_n,
     input       go,
     output reg  finish,
-    output reg  data,
+    output      data,
+    output reg  load,
     // signal to control I2C
     input       sda,
     output reg  scl
 );
 
-// 3-bit counter: when leave IDLE start counting
-reg [2:0] counter;
+// module to read 1-bit
+reg       read_bit_go;
+wire      read_bit_finish;
+wire      scl_o;
+
+assign scl = scl_o;
+assign load = read_bit_finish;
+
+I2C_master_read_bit(
+    .clock(clock),
+    .reset_n(reset_n),
+    .go(read_bit_go),
+    .finish(read_bit_finish),
+    .data(data),
+    .sda(sda)
+    .scl(scl_o),
+);
+
+// 4-bit counter: when leave IDLE start counting
+reg [3:0] counter;
 always @(posedge clock ) begin
     if((go == 1'b1) && (finish == 1'b0)) begin
-        if(counter == 3'b111)
-            counter <= 3'b000;
+        if(read_bit_finish) begin
+	      if(counter == 4'b1111)
+	          counter <= 4'b0000;
+	      else
+	          counter <= counter + 1'b1;
+        end
         else
-            counter <= counter + 1'b1;
+            counter <= counter;
     end
     else
-        counter <= 3'b000;
+        counter <= 4'b0000;
 end
 
 // state
 parameter IDLE = 1'b0;
-parameter READ_BIT = 1'b1;
+parameter READ_BYTE = 1'b1;
 
 // state varibele
 reg       state_next;
@@ -46,13 +69,13 @@ always @(*) begin
         IDLE:
             begin
                 if((go == 1'b1) && (finish == 1'b0))
-                    state_next = READ_BIT;
+                    state_next = READ_BYTE;
                 else
                     state_next = IDLE;
             end
-        READ_BIT:
+        READ_BYTE:
             begin
-                if(counter == 3'b110)
+                if(read_bit_finish && (counter == 4'b0111))
                     state_next = IDLE;
                 else
                     state_next = state_current;
@@ -65,7 +88,7 @@ end
 always @(*) begin
     if(!reset_n) begin
         data = 1'b0;
-	  scl = 1'b1;
+	    scl = 1'b1;
         finish = 1'b0;
     end
     else begin
@@ -74,26 +97,14 @@ always @(*) begin
                 begin
                     finish = 1'b0;
                 end
-            READ_BIT:
-                case(counter)
-                    3'b000, 3'b001, 3'b010, 3'b011: begin
-                        scl = 1'b0;
-                        finish = 1'b0;
-                    end
-                    3'b100: begin
-                        scl = 1'b1;
-                        finish = 1'b0;
-                    end
-                    3'b101: begin
-                        scl = 1'b1;
-                        data = sda;
-                        finish = 1'b0;
-                    end
-                    3'b110: begin
-                        scl = 1'b1;
+            READ_BYTE:
+                begin
+                    if(read_bit_finish && (counter == 4'b0111))
                         finish = 1'b1;
+                    else begin
+                        data = data_get;
                     end
-                endcase
+                end
         endcase
     end
 end

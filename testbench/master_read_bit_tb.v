13779ac872d3b7a12d808c4e13ba4a87e9974620
insanity_by <insanity_by@outlook.com>
2023-11-21 04:17:54 +0800

    rewrite master-read-bit and testbench

--- testbench/master_read_bit_tb.v
+++ testbench/master_read_bit_tb.v
@@ -2,45 +2,52 @@
 
 module testbench();
     reg clk, rst_n, go_test;
-    reg       sda_test;
-    wire finish_test, scl_test, data_test, error_test;
+    wire data_test, finish_test, error_test, scl_test;
+    reg sda_test;
+    
+    // test data to write
+    parameter test_data_value = 32'h1357_9bdf;
+    // test number
+    parameter test_number = 32;
+    reg [4:0] current_test_number;
+    reg [4:0] error_count;
     
     // instantiate the detector
     I2C_master_read_bit test_module(
     .clock(clk),
     .reset_n(rst_n),
     .go(go_test),
-    .finish(finish_test),
     .data(data_test),
+    .finish(finish_test),
     .error(error_test),
-    .sda(sda_test),
-    .scl(scl_test)
+    .scl(scl_test),
+    .sda(sda_test)
     );
     
-    // use fsdb to save wave
+    // use fsdb/vcd or vcd to save wave
     initial begin
         `ifdef fsdbdump
-        $display("\n*** fsdb file dump is turned on ***\n");
+        $display("\n******** fsdb file dump is turned on ******** \n");
         $fsdbDumpfile("wave.fsdb");
         $fsdbDumpvars(0);
         #100000
         $fsdbDumpoff;
         `endif
         `ifdef vcddump
-        $display("\n*** vcd file dump is turned on ***\n");
+        $display("******** vcd file dump is turned on******** ");
         $dumpfile("wave.vcd");
         $dumpvars(0);
-        #1000000
+        #100000
         $dumpoff;
         `endif
     end
     
-    // initialize inputs and create clock
+    // generate clock and reset
     initial begin
-        clk      = 0;
-        rst_n    = 1;
-        go_test  = 0;
-        sda_test = 1;
+        clk       = 0;
+        rst_n     = 1;
+        #10 rst_n = 0;
+        #10 rst_n = 1;
         forever
             #10 clk = ~clk;
     end
@@ -59,41 +66,78 @@ module testbench();
     end
     
     // write data to test module
+    reg [31:0] data_to_write;
+    reg last_write;
+    always @(*) begin
+        sda_test = data_to_write[31];
+    end
+    // save last data to check
     always @(posedge clk or negedge rst_n) begin
-        if (!rst_n)
-            sda_test <= 1'b1;
-        else if (detect)
-            sda_test <= ~sda_test;
-        else
-            sda_test <= sda_test;
+        if (!rst_n) begin
+            last_write <= 1'b0;
+        end
+        else begin
+            last_write <= sda_test;
+        end
+    end
+    // load data
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            data_to_write <= test_data_value;
+        end
+        else if (detect) begin
+            data_to_write <= {data_to_write[30:0], data_to_write[31]};
+        end
+        else begin
+            data_to_write <= data_to_write;
+        end
     end
     
-    
+    // start master_read_bit
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            go_test             <= 1'b1;
+            current_test_number <= 4'b0000;
+            error_count         <= 4'b0000;
+        end
+        else if (finish_test) begin
+            go_test             <= 1'b0;
+            current_test_number <= current_test_number + 1;
+            if (!error_test) begin
+                if (data_test == last_write) begin
+                    $display("--%02d-PASS-- write/read: %b/%b", current_test_number, last_write, data_test);
+                    error_count <= error_count;
+                end
+                else begin
+                    $display("--%02d-FAIL-- write/read: %b/%b", current_test_number, last_write, data_test);
+                    error_count <= error_count + 1;
+                end
+            end
+            else begin
+                $display("--%02d-FAIL-- error while reading");
+                error_count <= error_count + 1;
+            end
+        end
+        else begin
+            go_test             <= 1'b1;
+            current_test_number <= current_test_number;
+        end
+    end
+    // prompt and log
     initial begin
-        #10 rst_n = 0;
-        #10 rst_n = 1;
-        
-        // read 1st
-        go_test = 1;
-        wait(finish_test);
-        #1 $display("get: %b",data_test);
-        go_test = 0;
-        
-        // read 2nd
-        wait(!finish_test);
-        go_test = 1;
-        wait(finish_test);
-        #1 $display("get: %b",data_test);
-        go_test = 0;
-        
-        // read 3rd
-        wait(!finish_test);
-        go_test = 1;
-        wait(finish_test);
-        #1 $display("get: %b",data_test);
-        go_test = 0;
-        
-        #100 $finish;
+        $display("******** 'master_read_bit' module test started ********");
+        // wait till finished
+        wait(current_test_number == test_number - 1);
+        #100
+        $display("-------------------------------------------------");
+        if (error_count == 0) begin
+            $display("result: passed with %02d errors in %02d tests", error_count, test_number);
+        end
+        else begin
+            $display("result: failed with %02d errors in %02d tests", error_count, test_number);
+        end
+        $display("******** 'master_read_bit' module test finished ********");
+        $finish;
     end
     
 endmodule
--- verilog/master_read_bit.v
+++ verilog/master_read_bit.v
@@ -1,40 +1,77 @@
 module I2C_master_read_bit (input clock,
                             input reset_n,
-                            input go,
-                            output reg data,
-                            output reg finish,
-                            output reg error,
-                            input sda,
-                            output reg scl);
+                            input go,          // enable signal for module
+                            output reg data,   // data read from I2C bus
+                            output reg finish, // indicates completion of reading
+                            output reg error,  // indicates an error during reading
+                            output reg scl,
+                            input sda);
     
-    // 3-bit counter: drive the finite state machine
-    reg [2:0] counter;
+    // 3-bit counter for driving the FSM
+    reg [2:0] counter;  // current counter value
+    wire counter_en;    // enable signal for counter
+    // generate enable signal for counter
+    assign counter_en = go && (~finish);
+    // counter
     always @(posedge clock or negedge reset_n) begin
-        if (!reset_n)
+        if (!reset_n) begin
             counter <= 3'b000;
-        else if ((go == 1'b1) && (finish == 1'b0)) begin
+        end
+        else if (counter_en) begin
             if (counter == 3'b111)
                 counter <= 3'b000;
             else
                 counter <= counter + 1'b1;
         end
-        else
+        else begin
             counter <= 3'b000;
+        end
     end
     
-    // output, sequential circuit to handle race and hazard
+    // output
+    // master device controls scl during reading, combinational circuit
+    always @(*) begin
+        if (!reset_n)
+            scl = 1'b1;
+        else if (counter_en)
+            case(counter)
+                3'b000, 3'b001, 3'b010, 3'b011:
+                scl = 1'b0;
+                3'b100, 3'b101, 3'b110, 3'b111:
+                scl = 1'b1;
+            endcase
+        else
+            scl = 1'b1;
+    end
+    
+    // sample sda, sequential circuit
     // each bit is divided 8 parts, x means no sample, s means sample
     // +--------------+-----+---+---+---+---+---+---+---+---+
     // | set value    | scl | 0   0   0   0 | 1   1   1   1 |
     // | sample value | sda | x   x   x   x | s   s   s   s |
     // +--------------+-----+---+---+---+---+---+---+---+---+
-    reg [2:0] sample_value;
+    reg [2:0] sample_value; // add up the 4 sample values
+    always @(posedge clock or negedge reset_n) begin
+        if (!reset_n)
+            sample_value <= 3'b000;
+        else
+            case(counter)
+                3'b000, 3'b001, 3'b010, 3'b011:
+                sample_value <= 3'b000;
+                3'b100, 3'b101, 3'b110, 3'b111:
+                sample_value <= sample_value + sda;
+            endcase
+    end
+    
+    // generate data, error and finish, sequential circuit
     always @(posedge clock or negedge reset_n) begin
         if (!reset_n) begin
-            data  <= 1'b0;
-            error <= 1'b0;
+            finish <= 1'b0;
+            data   <= 1'b0;
+            error  <= 1'b0;
         end
         else if (counter == 3'b111) begin
+            finish <= 1'b1;
             case(sample_value)
                 3'b000, 3'b001: begin
                     data  <= 1'b0;
@@ -50,31 +87,11 @@ module I2C_master_read_bit (input clock,
                 end
             endcase
         end
-            end
-            always @(posedge clock or negedge reset_n) begin
-                if (!reset_n) begin
-                    scl          <= 1'b1;
-                    sample_value <= 4'b0000;
-                    finish       <= 1'b0;
-                end
-                else begin
-                    case(counter)
-                        3'b000, 3'b001, 3'b010, 3'b011: begin
-                            scl          <= 1'b0;
-                            sample_value <= 4'b0000;
-                            finish       <= 1'b0;
-                        end
-                        3'b100, 3'b101, 3'b110: begin
-                            scl          <= 1'b1;
-                            sample_value <= sample_value + sda;
-                            finish       <= 1'b0;
-                        end
-                        3'b111: begin
-                            scl          <= 1'b1;
-                            sample_value <= sample_value + sda;
-                            finish = 1'b1;
-                        end
-                    endcase
-                end
-            end
-            endmodule
+        else begin
+            finish <= 1'b0;
+            data   <= 1'b0;
+            error  <= 1'b0;
+        end
+    end
+    
+endmodule

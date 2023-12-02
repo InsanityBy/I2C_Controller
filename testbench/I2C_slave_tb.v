f46a501e19f317fc04846be88fce5de37640d6ad
insanity_by <insanity_by@outlook.com>
2023-12-02 18:32:57 +0800

    add combined mode test for slave in testbench

--- testbench/I2C_slave_tb.v
+++ testbench/I2C_slave_tb.v
@@ -333,6 +333,148 @@ always @(posedge clk or negedge rst_n) begin
     end
 end
 
+
+// test slave combined mode, read + write
+always @(posedge clk or negedge rst_n) begin
+    if(!rst_n) begin
+        data_to_write <= test_data_value;
+        sda_in <= 1'b1;
+        bit_counter <= 32'b0;
+    end
+    else if(test_start && test_control == 2'b11) begin
+        if (bit_counter == 32'b0) begin
+            if (scl_in) begin    // write start to module
+                sda_in <= 1'b0;
+                bit_counter <= bit_counter + 1;
+            end
+        end
+        else if (bit_counter <= 32'd7) begin // write address to module
+            if (scl_in_falling_edge) begin
+                sda_in <= module_address[7 - bit_counter];
+                bit_counter <= bit_counter + 1;
+            end
+        end
+        else if (bit_counter == 32'd8) begin // write read/write flag to module
+            if (scl_in_falling_edge) begin
+                sda_in <= 1'b1;
+                bit_counter <= bit_counter + 1;
+            end
+        end
+        else if (bit_counter == 32'd9) begin // module write ack
+            if (scl_in_falling_edge) begin
+                bit_counter <= bit_counter + 1;
+            end
+        end
+        else if (bit_counter == 32'd10) begin    // check ack from module
+            if (scl_in_rising_edge) begin
+                bit_counter <= bit_counter + 1;
+            end
+        end
+        else if (bit_counter <= (transfer_byte_number * 10 + 10)) begin    // write data to module
+            if (((bit_counter % 10) >= 32'd1) && ((bit_counter % 10) <= 32'd8)) begin // 8 bits
+                if (scl_in_falling_edge) begin
+                    sda_in <= data_to_write[transfer_byte_number * 8 - (bit_counter % 10)];
+                    bit_counter <= bit_counter + 1;
+                end
+            end
+            else if ((bit_counter % 10)  == 32'd9) begin // module write ack
+                if (scl_in_falling_edge) begin
+                    bit_counter <= bit_counter + 1;
+                end
+            end
+            else if ((bit_counter % 10)  == 32'd0) begin    // check ack from module
+                if (scl_in_rising_edge) begin
+                    if (sda_out) begin  // NACK, stop transfer
+                        bit_counter <= transfer_byte_number * 10 + 11;
+                    end
+                    else begin  // prepare next byte data to write to module
+                        data_to_write = {data_to_write[transfer_byte_number * 8 - 9 : 0], 8'b0};
+                        bit_counter <= bit_counter + 1;
+                    end
+                end
+            end
+        end
+        else if (bit_counter == (transfer_byte_number * 10 + 11)) begin    // prepare to restart
+            if (scl_in_falling_edge) begin
+                sda_in <= 1'b1;
+                bit_counter <= bit_counter + 1;
+            end
+        end
+        else if (bit_counter == (transfer_byte_number * 10 + 12)) begin    //write restart to module
+            if(scl_in) begin
+                sda_in <= 1'b0;
+                bit_counter <= bit_counter + 1;
+            end
+        end
+        else if (bit_counter <= (transfer_byte_number * 10 + 19)) begin // write address to module
+            if (scl_in_falling_edge) begin
+                sda_in <= module_address[7 - bit_counter];
+                bit_counter <= bit_counter + 1;
+            end
+        end
+        else if (bit_counter == (transfer_byte_number * 10 + 20)) begin // write read/write flag to module
+            if (scl_in_falling_edge) begin
+                sda_in <= 1'b0;
+                bit_counter <= bit_counter + 1;
+            end
+        end
+        else if (bit_counter == (transfer_byte_number * 10 + 21)) begin // module write ack
+            if (scl_in_falling_edge) begin
+                bit_counter <= bit_counter + 1;
+            end
+        end
+        else if (bit_counter == (transfer_byte_number * 10 + 22)) begin    // check ack from module
+            if (scl_in_rising_edge) begin
+                bit_counter <= bit_counter + 1;
+            end
+        end
+        else if (bit_counter <= (transfer_byte_number * 20 + 22)) begin    // module write data
+            if (((bit_counter % 10) >= 32'd3) && ((bit_counter % 10) <= 32'd9)) begin  // read data from module
+                if (scl_in_rising_edge) begin
+                    byte_read <= {byte_read[6:0], sda_out};
+                    bit_counter <= bit_counter + 1;
+                end
+            end
+            else if ((bit_counter % 10)  == 32'd0) begin // read last bit and load next byte to module
+                if (scl_in_rising_edge) begin
+                    byte_read <= {byte_read[6:0], sda_out};
+                    data_to_load <= {data_to_load[transfer_byte_number * 8 - 9 : 0], 8'b0}; // load data
+                    bit_counter <= bit_counter + 1;
+                end
+            end
+            else if ((bit_counter % 10)  == 32'd1) begin // write ack to module
+                if (scl_in_falling_edge) begin
+                    if (bit_counter == transfer_byte_number * 10 + 9) begin // write NACK
+                        sda_in <= 1'b1;
+                    end
+                    else begin
+                        sda_in <= 1'b0;
+                    end
+                    bit_counter <= bit_counter + 1;
+                end
+            end
+            else if ((bit_counter % 10)  == 32'd2) begin    // wait module check ack
+                if (scl_in_rising_edge) begin
+                    bit_counter <= bit_counter + 1;
+                end
+            end
+        end
+        else if (bit_counter == (transfer_byte_number * 20 + 23)) begin    // prepare to stop
+            if (scl_in_falling_edge) begin
+                sda_in <= 1'b0;
+                bit_counter <= bit_counter + 1;
+            end
+        end
+        else if (bit_counter == (transfer_byte_number * 20 + 24)) begin    // stop
+            if(scl_in) begin
+                sda_in <= 1'b1;
+                bit_counter <= 32'b0;
+                test_start <= 1'b0;
+            end
+        end
+    end
+end
+
 // enable submodule
 always @(posedge clk or negedge rst_n) begin
     if(!rst_n) begin
@@ -363,7 +505,6 @@ initial begin
     $display("--3-- write data test");
 
     wait(!test_start);
-    #500 $finish;
     test_control = 2'b11;
     #500 test_start = 1'b1;
     $display("--4-- combined mode test");

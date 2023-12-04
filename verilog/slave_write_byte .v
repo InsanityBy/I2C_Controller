9f8117afee5edbce0c58ea2c331211f3c6da6bdb
insanity_by <insanity_by@outlook.com>
2023-12-05 05:36:46 +0800

    refactor: change the logic of enable, finish and error signals

--- verilog/slave_write_byte .v
+++ verilog/slave_write_byte .v
@@ -1,100 +1,110 @@
 module I2C_slave_write_byte (
-           input clock,
-           input reset_n,
-           input enable,    // enable signal, expected to be a pulse at scl falling edge
-           input [7:0] data,// data write to I2C bus
-           output finish,   // finish signal
-           input scl,
-           output sda);
+    input clk,
+    input rst_n,
+    input byte_write_en,  // enable, expected to be high at or after scl falling edge
+    input [7:0] byte_write_i,  // 1-byte data write to I2C bus
+    output reg byte_write_finish,
+    input scl_i,
+    output sda_o
+);
 
-// instantiate I2C_slave_write_bit
-wire write_bit_enable, write_bit_data, write_bit_finish;
-I2C_slave_write_bit write_bit(
-                        .clock(clock),
-                        .reset_n(reset_n),
-                        .enable(write_bit_enable),
-                        .data(write_bit_data),
-                        .finish(write_bit_finish),
-                        .scl(scl),
-                        .sda(sda)
-                    );
+    // instantiate I2C_slave_write_bit
+    wire bit_write_en, bit_write_i, bit_write_finish;
+    I2C_slave_write_bit write_bit (
+        .clk             (clk),
+        .rst_n           (rst_n),
+        .bit_write_en    (bit_write_en),
+        .bit_write_i     (bit_write_i),
+        .bit_write_finish(bit_write_finish),
+        .scl_i           (scl_i),
+        .sda_o           (sda_o)
+    );
 
-// detect scl falling edge
-reg scl_last_state;
-wire scl_falling_edge;
-// save scl last state
-always @(posedge clock or negedge reset_n) begin
-    if (!reset_n) begin
-        scl_last_state <= 1'b1;
-    end
-    else begin
-        scl_last_state <= scl;
-    end
-end
-// scl falling edge: 1 -> 0
-assign scl_falling_edge = scl_last_state && (~scl);
-
-// 3-bit counter for writing 1-byte data bit by bit
-reg [2: 0] counter;
-always @(posedge clock or negedge reset_n ) begin
-    if (!reset_n) begin
-        counter <= 3'b000;
-    end
-    else if(write_bit_finish) begin
-        if(counter == 3'b111) begin
-            counter <= 3'b000;
+    // detect scl_i falling edge
+    reg  scl_last;
+    wire scl_fall;
+    // save scl_i last state
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            scl_last <= 1'b1;
         end
         else begin
-            counter <= counter + 1;
+            scl_last <= scl_i;
         end
     end
-    else begin
-        counter <= counter;
-    end
-end
+    // scl_i falling edge: 0 -> 1
+    assign scl_fall = scl_last && (~scl_i);
 
-// load data to shift register
-reg [7:0] shift_register;
-always @(posedge clock or negedge reset_n) begin
-    if (!reset_n) begin
-        shift_register <= 8'b0000_0000;
+    // 3-bit counter for writing 1-byte data bit by bit
+    reg [2:0] counter;
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            counter <= 3'b000;
+        end
+        else if (!byte_write_en) begin
+            counter <= 3'b000;
+        end
+        else if (bit_write_finish) begin
+            if (counter == 3'b111) begin
+                counter <= 3'b000;
+            end
+            else begin
+                counter <= counter + 1;
+            end
+        end
+        else begin
+            counter <= counter;
+        end
     end
-    else if ((~enabled) && scl_falling_edge) begin
-        shift_register <= data;
-    end
-    else if (write_bit_finish) begin
-        shift_register <= {shift_register[6:0], 1'b0};
-    end
-    else begin
-        shift_register <= shift_register;
-    end
-end
 
-// track whether module has been enabled to prevent unexpected read_bit_enable
-reg enabled;
-always @(posedge clock or negedge reset_n) begin
-    if (!reset_n) begin
-        enabled <= 1'b0;
+    // track whether module has been enabled to prevent unexpected finish flag
+    reg enabled;
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            enabled <= 1'b0;
+        end
+        else if (byte_write_en) begin
+            enabled <= 1'b1;
+        end
+        else if ((~byte_write_en) || ((counter == 3'b111) && bit_write_finish)) begin
+            enabled <= 1'b0;
+        end
+        else begin
+            enabled <= enabled;
+        end
     end
-    else if (enable && (~scl)) begin
-        enabled <= 1'b1;
+
+    // bit_write_en
+    assign bit_write_en = byte_write_en;
+
+    // load data from shift register to bit_write_i
+    reg [7:0] shift_register;
+    assign bit_write_i = shift_register[7];
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            shift_register <= 8'b0000_0000;
+        end
+        else if (~enabled) begin
+            shift_register <= byte_write_i;
+        end
+        else if (bit_write_finish) begin
+            shift_register <= {shift_register[6:0], 1'b0};
+        end
+        else begin
+            shift_register <= shift_register;
+        end
     end
-    else if((counter == 3'b111) && write_bit_finish) begin
-        enabled <= 1'b0;
+
+    // byte_write_finish, when 8-bit data writing finished
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            byte_write_finish <= 1'b0;
+        end
+        else if (bit_write_finish && (counter == 3'b111)) begin
+            byte_write_finish <= 1'b1;
+        end
+        else begin
+            byte_write_finish <= 1'b0;
+        end
     end
-    else begin
-        enabled <= enabled;
-    end
-end
-
-// generate write_bit_enable
-// first bit is enabled by enable signal, others are enabled by scl falling edge
-assign write_bit_enable = enable || (scl_falling_edge && enabled);
-
-// generate write_bit_data
-assign write_bit_data = shift_register[7];
-
-// generate finish
-assign finish = write_bit_finish && (counter == 3'b111);
-
 endmodule

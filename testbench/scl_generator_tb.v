c26e86a8dddb0f64e4d2367475205605587aded4
insanity_by <insanity_by@outlook.com>
2023-12-21 06:52:47 +0800

    refactor: move setting scl_div external, modify scl stretch detect

--- testbench/scl_generator_tb.v
+++ testbench/scl_generator_tb.v
@@ -1,26 +1,6 @@
 `timescale 1ns / 10ps
 
 module testbench ();
-    reg clk, rst_n, scl_en, scl_wait;
-    reg  [7:0] scl_div;
-    wire [7:0] scl_div_cur;
-    wire       scl_stretched;
-    reg        scl_ctrl;
-    wire scl, scl_o;
-
-    // instantiate the submodule
-    scl_generator test_module (
-        .clk          (clk),
-        .rst_n        (rst_n),
-        .scl_en       (scl_en),
-        .scl_wait     (scl_wait),
-        .set_scl_div  (scl_div),
-        .scl_div      (scl_div_cur),
-        .scl_stretched(scl_stretched),
-        .scl_i        (scl),
-        .scl_o        (scl_o)
-    );
-
     // use fsdb/vcd or vcd to save wave
 `ifdef fsdbdump
     initial begin
@@ -39,63 +19,80 @@ module testbench ();
     end
 `endif
 
+
+    // test parameters
+    parameter clk_period = 20;
+    parameter scl_div1 = 4;
+    parameter scl_div2 = 4;
+
+    // signals
+    reg clk, rst_n, scl_en, scl_wait;
+    wire scl_stretched;
+    reg  scl_ctrl;
+    wire scl, scl_o;
+
+    // instantiate the module under test
+    scl_generator test_module (
+        .clk          (clk),
+        .rst_n        (rst_n),
+        .scl_en       (scl_en),
+        .scl_wait     (scl_wait),
+        .scl_div      (scl_div1),
+        .scl_stretched(scl_stretched),
+        .scl_i        (scl),
+        .scl_o        (scl_o)
+    );
+
     // generate clock and reset
     initial begin
-        clk   = 0;
-        rst_n = 1;
-        #10 rst_n = 0;
-        #10 rst_n = 1;
-        forever #10 clk = ~clk;
+        clk   = 1'b0;
+        rst_n = 1'b1;
+        #clk_period rst_n = 1'b0;
+        #clk_period rst_n = 1'b1;
+        forever #(clk_period / 2) clk = ~clk;
+    end
+
+    // generate another scl
+    integer scl_cnt;
+    always @(posedge clk) begin
+        if (!scl_en) begin
+            scl_ctrl <= 1'b1;
+            scl_cnt  <= 0;
+        end
+        else if (scl_ctrl != scl) begin
+            scl_ctrl <= scl_ctrl;
+            scl_cnt  <= scl_cnt;
+        end
+        else if (scl_cnt == (scl_div2 - 1)) begin
+            scl_ctrl <= 1'b0;
+            scl_cnt  <= scl_cnt + 1;
+        end
+        else if (scl_cnt == (scl_div2 * 2 - 1)) begin
+            scl_ctrl <= 1'b1;
+            scl_cnt  <= 0;
+        end
+        else begin
+            scl_ctrl <= scl_ctrl;
+            scl_cnt  <= scl_cnt + 1;
+        end
     end
 
     // connect scl
     assign scl = scl_o && scl_ctrl;
 
-    // detect scl rising and falling edge
-    reg  scl_last;
-    wire scl_fall;
-    // save scl last state
-    always @(posedge clk or negedge rst_n) begin
-        if (!rst_n) begin
-            scl_last <= 1'b1;
-        end
-        else begin
-            scl_last <= scl;
-        end
-    end
-    assign scl_fall = scl_last && (~scl);
-
-    // test clock divisor
+    // test scl synchronization
     initial begin
         scl_ctrl = 1;
         scl_wait = 0;
-        scl_div  = 0;
         scl_en   = 0;
-
-        // enable module
-        #20 scl_en = 1;
-
-        // set scl_div when module enabled
-        // scl_div should not be loaded
-        #20 scl_div = 8'hff;
-
-        // set scl_div when module disabled
-        #200 scl_en = 0;
-        #20 scl_div = 8'h01;
-        #100 scl_en = 1;
-
-        // simulate scl being stretched
-        #100 wait (scl_fall);
-        #20 scl_ctrl = 0;
-        #100 scl_ctrl = 1;
-
-        // simulate stretching scl to wait
-        #100 wait (scl_fall);
-        #20 scl_wait = 1;
-        #100 scl_wait = 0;
-
-        #100 scl_en = 0;
-        $finish;
+        // stat test
+        @(posedge clk) #1 scl_en = 1;
+        #1000;
+        // test scl wait
+        @(negedge scl_o) #1 scl_wait = 1;
+        #1000 scl_wait = 0;
+        // finish
+        #10000 $finish;
     end
 
 endmodule
--- verilog/scl_generator.v
+++ verilog/scl_generator.v
@@ -4,7 +4,7 @@
  *
  * file name: scl_generator.v
  * create date: 2023.12.09
- * last modified date: 2023.12.12
+ * last modified date: 2023.12.21
  *
  * design name: I2C_controller
  * module name: scl_generator
@@ -19,6 +19,9 @@
  *     initial version
  * V1.1 - 2023.12.12
  *     rename signals
+ * V1.2 - 2023.12.21
+ *     refactor: move setting scl_div external
+ *               modify scl stretch detect
  */
 
 module scl_generator (
@@ -27,32 +30,13 @@ module scl_generator (
     // control
     input scl_en,
     input scl_wait,  // stretch scl to wait, can ONLY be set when scl low
-    input [7:0] set_scl_div,  // 1~255, f_{scl_o} = f_{clk}/(2*(scl_div+1))
+    input [7:0] scl_div,  // 1~255, f_{scl_o} = f_{clk}/(2*(scl_div+1))
     // status
-    output reg [7:0] scl_div,  // current scl_div value
-    output reg scl_stretched,
+    output reg scl_stretched,  // scl is stretched by other devices
     // I2C
     input scl_i,
     output reg scl_o
 );
-
-    // set scl divisor
-    always @(posedge clk or negedge rst_n) begin
-        if (!rst_n) begin
-            scl_div <= 8'h01;
-        end
-        else if (!scl_en) begin  // scl_div can ONLY be set when module disabled
-            if (set_scl_div == 8'b0) begin
-                scl_div <= 8'h01;
-            end
-            else begin
-                scl_div <= set_scl_div;
-            end
-        end
-        else begin
-            scl_div <= scl_div;
-        end
-    end
 
     // counter to divide clock and generate scl_o
     reg [8:0] scl_cnt;
@@ -82,78 +66,17 @@ module scl_generator (
         scl_o = ~scl_cnt[8];
     end
 
-    // detect scl_o rising edge
-    reg scl_last, scl_rise;
-    // save scl_o last state
+    // detect scl being stretched
     always @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
-            scl_last <= 1'b1;
+            scl_stretched <= 1'b0;
+        end
+        else if (scl_o != scl_i) begin
+            scl_stretched <= 1'b1;
         end
         else begin
-            scl_last <= scl_o;
+            scl_stretched <= 1'b0;
         end
-    end
-    // scl_o rising edge: 0 -> 1
-    always @(*) begin
-        scl_rise = (~scl_last) && scl_o;
-    end
-
-    // finite state machine to check clock stretch
-    // state encode
-    parameter IDLE = 1'b0;
-    parameter WAIT = 1'b1;
-
-    // state variable
-    reg state_next, state_current;
-
-    // state transfer, sequential circuit
-    always @(posedge clk or negedge rst_n) begin
-        if (!rst_n) begin
-            state_current <= IDLE;
-        end
-        else begin
-            state_current <= state_next;
-        end
-    end
-
-    // state switch, combinational circuit
-    always @(*) begin
-        case (state_current)
-            IDLE: begin
-                if (scl_rise && (~scl_i)) begin  // scl is stretched
-                    state_next = WAIT;
-                end
-                else begin
-                    state_next = IDLE;
-                end
-            end
-            WAIT: begin
-                if (scl_i) begin  // wait till scl_i released
-                    state_next = IDLE;
-                end
-                else begin
-                    state_next = WAIT;
-                end
-            end
-            default: begin
-                state_next = IDLE;
-            end
-        endcase
-    end
-
-    // state output, combinational circuit
-    always @(*) begin
-        case (state_current)
-            IDLE: begin
-                scl_stretched = 1'b0;
-            end
-            WAIT: begin
-                scl_stretched = 1'b1;
-            end
-            default: begin
-                scl_stretched = 1'b0;
-            end
-        endcase
     end
 
 endmodule
